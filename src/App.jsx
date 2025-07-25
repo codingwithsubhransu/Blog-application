f816d5cb9099371c528911244b308a46c2ba8422
Subhransu Sekhar Rout <155973959+subhransu-max@users.noreply.github.com>
2025-07-25 16:10:10 +0530

    added store and redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import { Header ,Footer } from './Components'
+
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch() 
+  useEffect(() => {
+
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login(userData))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
 
 
-  return (
-    <>
-      <h1>A Blog with appwrite</h1>
-    </>
-  )
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap  bg-gray-400'>
+      <div>
+        <Header />
+        <main>
+          Todo: {/* Outlet */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
